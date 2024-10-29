33463c73f994e6ccb21ea538f31c352acd3cd740
Pupi <95034@sistemas.frc.utn.edu.ar>
2024-10-29 18:22:47 -0300

    ...

--- microservice-logica/src/main/java/com/microservice/logica/utils/Coordenada.java
+++ microservice-logica/src/main/java/com/microservice/logica/utils/Coordenada.java
@@ -9,19 +9,15 @@ public class Coordenada {
     private double longitud; // <- x
     private double latitud; // <- y
 
-    public double calcularDistancia(Coordenada punto){
+    public double calcularDistancia(Coordenada punto) {
         double deltaX = (longitud - punto.getLongitud());
         double deltaY = (latitud - punto.getLatitud());
         //calculo la distancia aplicando pitagoras
         return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
     }
-<<<<<<< HEAD
-    public boolean pasoDistanciaMaxima(Coordenada punto, float distanciaMax){
-        float distancia = calcularDistancia(punto);
-=======
-    public boolean pasoDistanciaMaxima(Coordenada punto, double distanciaMax){
+
+    public boolean pasoDistanciaMaxima(Coordenada punto, double distanciaMax) {
         double distancia = calcularDistancia(punto);
->>>>>>> 0d83a86b1c0eb41e95045e6dd46c6f6617cc9d55
         return distancia > distanciaMax;
     }
 }
--- microservice-logica/src/test/java/com/microservice/logica/Main.java
+++ microservice-logica/src/test/java/com/microservice/logica/Main.java
@@ -1,30 +1,7 @@
 package com.microservice.logica;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.google.gson.GsonBuilder;
-import com.microservice.logica.controladores.DTO.DTONotificacionAdvertencia;
-import com.google.gson.Gson;
-import com.microservice.logica.utils.AdaptadorFecha;
-
-import java.time.LocalDateTime;
-
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        /*
-        Coordenada noreoeste = new Coordenada(-34,50);
-        Coordenada sureste = new Coordenada(-28,40);
-        ZonaPeligrosa zonaPeligrosa = new ZonaPeligrosa(noreoeste, sureste);
-        Coordenada coordenada = new Coordenada(-30,50);
-        System.out.println(zonaPeligrosa.contiene(coordenada));
-         */
-        Coordenada a = new Coordenada(3,1);
-        Coordenada b = new Coordenada(5,2);
-        System.out.println(a.calcularDistancia(b));
-        System.out.println(a.pasoDistanciaMaxima(b,2.235F));
-=======
 
-
->>>>>>> 0d83a86b1c0eb41e95045e6dd46c6f6617cc9d55
     }
 }
